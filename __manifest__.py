edb2599c036011275ad72a043d75bfbfc2c19d4d
Marc Wimmer <marc@itewimmer.de>
2024-05-21 21:24:51 +0200

    16.0

--- __manifest__.py
+++ __manifest__.py
@@ -12,5 +12,8 @@
     'external_dependencies': {'bin': [], 'python': []},
     'name': 'diagrams_net',
     'test': [],
-    'version': '16.0.1.0',
-    'web': True}
+    'version': '1.0',
+    'web': True,
+    'license': 'LGPL-3',
+    'scmUrl': 'https://github.com/Odoo-Ninjas/odoo-diagramsnet'
+}
